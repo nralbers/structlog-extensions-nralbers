ce03336d6c97a9ab44534435b438fe1d6b910112
Niels Albers <nralbers@gmail.com>
2019-08-20 21:20:43 +0200

    Refactor code and add test for use as part of processor chain

--- structlog_extensions/processors.py
+++ structlog_extensions/processors.py
@@ -31,37 +31,46 @@ class CombinedLogParser:
             dict: Modified event_dict
 
         """
-        if event_dict.get('logger') == self.loggerfilter:
-            message = event_dict['event']
+        try:
+            if logger and logger.name == self.loggerfilter:
+                message = event_dict['event']
+                res = self._parse_log(message)
 
-            parts = [
-                r'(?P<host>\S+)',  # host %h
-                r'\S+',  # indent %l (unused)
-                r'(?P<user>\S+)',  # user %u
-                r'\[(?P<time>.+)\]',  # time %t
-                r'"(?P<request>.+)"',  # request "%r"
-                r'(?P<status>[0-9]+)',  # status %>s
-                r'(?P<size>\S+)',  # size %b (careful, can be '-')
-                r'"(?P<referer>.*)"',  # referer "%{Referer}i"
-                r'"(?P<agent>.*)"',  # user agent "%{User-agent}i"
-            ]
-            pattern = re.compile(r'\s+'.join(parts) + r'\s*\Z')
-            m = pattern.match(message)
-            res = m.groupdict()
-
-            if res["user"] == "-":
-                res["user"] = None
-
-            res["status"] = int(res["status"])
-
-            if res["size"] == "-":
-                res["size"] = 0
-            else:
-                res["size"] = int(res["size"])
-
-            if res["referer"] == "-":
-                res["referer"] = None
-
-            event_dict.update(res)
+                event_dict.update(res)
+        finally:
+            pass
 
         return event_dict
+
+    def _parse_log(self, message):
+
+        parts = [
+            r'(?P<host>\S+)',  # host %h
+            r'\S+',  # indent %l (unused)
+            r'(?P<user>\S+)',  # user %u
+            r'\[(?P<time>.+)\]',  # time %t
+            r'"(?P<request>.+)"',  # request "%r"
+            r'(?P<status>[0-9]+)',  # status %>s
+            r'(?P<size>\S+)',  # size %b (careful, can be '-')
+            r'"(?P<referer>.*)"',  # referer "%{Referer}i"
+            r'"(?P<agent>.*)"',  # user agent "%{User-agent}i"
+        ]
+        pattern = re.compile(r'\s+'.join(parts) + r'\s*\Z')
+        m = pattern.match(message)
+        res = m.groupdict()
+        if res["user"] == "-":
+            res["user"] = None
+        res["status"] = int(res["status"])
+        if res["size"] == "-":
+            res["size"] = 0
+        else:
+            res["size"] = int(res["size"])
+        if res["referer"] == "-":
+            res["referer"] = None
+        return res
+
+
+
+
+
+
--- tests/test_combinedLogParser.py
+++ tests/test_combinedLogParser.py
@@ -1,15 +1,49 @@
 from unittest import TestCase
 from structlog_extensions.processors import CombinedLogParser
+import logging
+import sys
+import structlog
 
+
+
+logging.basicConfig(
+    format="%(message)s",
+    stream=sys.stdout,
+    level=logging.INFO,
+)
 
 class TestCombinedLogParser(TestCase):
     def test_parse_combined_log(self):
         logparser = CombinedLogParser("gunicorn.access")
-        logger = object()
-        method_name = "warn"
+        logger = logging.Logger("gunicorn.access")
+        method_name = "warning"
         event_dict = {
-            'event': '127.0.0.1 - - [05/Feb/2012:17:11:55 +0000] "GET / HTTP/1.1" 200 140 "-" "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.5 Safari/535.19"',
-            'logger': 'gunicorn.access', 'level': 'warn'}
-        self.assertEqual(logparser.parse_combined_log(logger, method_name, event_dict)['status'], 200, "Status code not extracted correctly")
+            'event': '127.0.0.1 - - [05/Feb/2012:17:11:55 +0000] "GET / HTTP/1.1" 200 140 "-" "Mozilla/5.0 (Windows '
+                     'NT 6.1; WOW64) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.5 Safari/535.19"'}
+        self.assertEqual(logparser.parse_combined_log(logger, method_name, event_dict)['status'], 200,
+                         "Status code not extracted correctly")
         self.assertEqual(logparser.parse_combined_log(logger, method_name, event_dict)['host'], "127.0.0.1",
                          "Host not extracted correctly")
+
+    def test_chain_processor(self):
+        logparser = CombinedLogParser("access")
+        structlog.configure(
+            processors=[
+                logparser.parse_combined_log,
+                structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+            ],
+            logger_factory=structlog.stdlib.LoggerFactory(),
+        )
+
+        formatter = structlog.stdlib.ProcessorFormatter(
+            processor=structlog.dev.ConsoleRenderer(colors=False),
+        )
+
+        handler = logging.StreamHandler()
+        handler.setFormatter(formatter)
+        root_logger = logging.getLogger()
+        root_logger.addHandler(handler)
+        root_logger.setLevel(logging.INFO)
+
+        logger = structlog.get_logger("access")
+        logger.warning('127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"')
