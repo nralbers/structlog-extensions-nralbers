8107930778ab6bfcfd333f492a39bb431db3a022
Niels Albers <nralbers@gmail.com>
2019-09-02 01:02:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup
 
+KEYWORDS = ["logging", "structured", "structure", "log","ecs","apache-combined-log"]
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
@@ -8,18 +9,33 @@ setup(
     name='structlog-extensions-nralbers',
     version='1.0.0',
     packages=['tests', 'structlog_extensions'],
-    url='https://github.com/nralbers/structlog-extensions-nralbers',
+    url='https://structlog-extensions-nralbers.readthedocs.io/en/latest/',
+    project_urls={
+        'Documentation': 'https://structlog-extensions-nralbers.readthedocs.io/en/latest/',
+        'Source': 'https://github.com/nralbers/structlog-extensions-nralbers',
+        'Tracker': 'https://github.com/nralbers/structlog-extensions-nralbers/issues',
+    },
     license='MIT',
     author='Niels Albers',
     author_email='nralbers@gmail.com',
     description='Processors for Structlog library',
+    install_requires=['structlog','user-agents','deepmerge'],
+    keywords=KEYWORDS,
     long_description=long_description,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
+    python_requires='~=3.5',
     classifiers=[
+        "Natural Language :: English",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy"
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Development Status :: 5 - Production/Stable",
-        "Topic :: System :: Logging"
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers"
     ],
 )
